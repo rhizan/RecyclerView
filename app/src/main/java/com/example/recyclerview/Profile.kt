c3a51a309ac67e1bf367164b3142f8d1076a6e16
Didan <rhizanmahadyan00@gmail.com>
2022-07-03 09:54:33 +0700

    Recylcerview APP

---
+++ app/src/main/java/com/example/recyclerview/Profile.kt
@@ -0,0 +1,23 @@
+package com.example.recyclerview
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.recyclerview.databinding.ActivityProfileBinding
+
+class Profile : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnback.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
